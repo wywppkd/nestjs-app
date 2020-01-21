3be1210dc4c598749a9108153bf8dbaa8a4901df
liyb <1055025755@qq.com>
2020-01-21 09:47:19 +0800

    feat: ejs config

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Post, Param, Body, Query } from '@nestjs/common';
+import { Controller, Get, Post, Param, Body, Query, Render } from '@nestjs/common';
 import { CreateCatDto } from './dto/create-cat.dto';
 
 @Controller('cats')
--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,12 @@ async function bootstrap() {
 
   // 配置静态资源目录(使用了Express)
   app.useStaticAssets(join(__dirname, "..", "public"));
+  
+  // 设置模板引擎ejs
+  app.setBaseViewsDir(join(__dirname, "..", "views")); // 放视图的文件
+  app.setViewEngine("ejs");
 
   await app.listen(3000);
+  console.log(`Nest Server is runing at http://127.0.0.1:3000`)
 }
 bootstrap();
